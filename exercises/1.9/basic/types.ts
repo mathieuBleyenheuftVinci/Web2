2b304b02c5df3d19ae6597ee1e1e4fa90bd5f15d
mathieuBleyenheuftVinci <165761155+mathieuBleyenheuftVinci@users.noreply.github.com>
2024-10-07 14:21:28 +0200

    nex ex1.9

--- exercises/1.9/basic/types.ts
+++ exercises/1.9/basic/types.ts
@@ -1,13 +1,9 @@
-interface Film {
-  id: number;
-  title: string;
-  director: string;
-  duration: number;
-  budget?: number;
-  description?: string;
-  imageUrl?: string;
+interface Text {
+  id: string;
+  content: string;
+  level: "easy" | "medium" | "hard";
 }
 
-type NewFilm = Omit<Film, "id">;
+type NewText = Omit<Text, "id">;
 
-export type { Film, NewFilm };
+export type { Text, NewText };
