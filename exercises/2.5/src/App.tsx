bf4107b3c9452f50a31ac5dcc70bb63711c580d6
mathieuBleyenheuftVinci <mathieu.bleyenheuft@student.vinci.be>
2024-10-25 09:42:35 +0200

    new ex2.5

--- exercises/2.5/src/App.tsx
+++ exercises/2.5/src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ClickCounter from "./ClickCounter"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const ClickCounter = () => {
-    console.log(`value of counter : ${count}`);
-    setCount((count) => count +1);
-  }
 
   return (
     <>
@@ -21,15 +16,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={(ClickCounter)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1></h1>
+      <ClickCounter
+        title="Amazing counter"
+        on10ClickMessage="You are a master in the art of clicking !"
+        onMouseOverMessage="Please click on me now !"
+      />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
@@ -37,4 +29,5 @@ function App() {
   )
 }
 
-export default App
+
+export default App;
