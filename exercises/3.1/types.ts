21ec143c11844882e70c79c4365427763e51af55
mathieuBleyenheuftVinci <mathieu.bleyenheuft@student.vinci.be>
2024-12-04 17:27:17 +0100

    init: ex3.1

---
+++ exercises/3.1/types.ts
@@ -0,0 +1,46 @@
+import { Request } from "express";
+
+interface Film {
+  id: number;
+  title: string;
+  director: string;
+  duration: number;
+  budget?: number;
+  description?: string;
+  imageUrl?: string;
+}
+
+type NewFilm = Omit<Film, "id">;
+
+interface AuthenticatedUser {
+  username: string;
+  token: string;
+}
+
+interface User {
+  id: number;
+  username: string;
+  password: string;
+}
+
+type PotentialUser = Omit<User, "id">;
+
+interface AuthenticatedRequest extends Request {
+  user?: User;
+}
+
+interface JwtPayload {
+  username: string;
+  exp: number; // Expiration time (in seconds since the epoch)
+  iat: number; // Issued at time (in seconds since the epoch)
+}
+
+export type {
+  Film,
+  NewFilm,
+  AuthenticatedUser,
+  User,
+  PotentialUser,
+  AuthenticatedRequest,
+  JwtPayload,
+};
