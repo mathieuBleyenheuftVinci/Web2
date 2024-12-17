4cb6d356e827788f75918b240788127e7fa034f0
mathieuBleyenheuftVinci <165761155+mathieuBleyenheuftVinci@users.noreply.github.com>
2024-12-17 10:34:00 +0100

    init: examenBlanc

--- exercises/3.3-4/src/components/Navbar.tsx
+++ exercises/3.3-4/src/components/Navbar.tsx
@@ -1,7 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 
-const NavBar = () => (
+interface NavBarProps {
+  authenticatedUser: MaybeAuthenticatedUser;
+  clearUser: () => void;
+}
+
+const NavBar = ({authenticatedUser, clearUser}: NavBarProps) => (
+  const navigate = useNavigate();
+
+  if(authenticatedUser) {
+    return(
+      
+    )
   <nav className="navbar">
     <Link to="/">Home</Link>
     <Link to="/cinemas">Cinemas</Link>
